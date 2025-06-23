6234e6b5ab5b0f78040186a049b3e93fd92efe39
David Bond <david.bond@panoramicdata.com>
2025-06-23 13:42:25 +0100

    Added image assessment using Llava.

--- Ollama.Api.Test/GenerateTests.cs
+++ Ollama.Api.Test/GenerateTests.cs
@@ -6,6 +6,8 @@ namespace Ollama.Api.Test;
 
 public class GenerateTests(Fixture fixture, ITestOutputHelper testOutputHelper) : Test(fixture, testOutputHelper)
 {
+	private const string LlavaModel = "llava:latest";
+
 	[Fact]
 	public async Task MinimalGenerate_Succeeds()
 	{
@@ -46,5 +48,46 @@ public class GenerateTests(Fixture fixture, ITestOutputHelper testOutputHelper) 
 			await OllamaClient.Generate.GenerateAsync(request, default);
 		});
 		ex.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+	}
+
+	[Fact]
+	public async Task Generate_DescribeImage_Succeeds()
+	{
+		// Arrange
+		var imageFileInfo = new FileInfo("Images/Ollama Logo.png");
+		imageFileInfo.Exists.Should().BeTrue("Test image file should exist for this test to run.");
+		var imageBytes = await File.ReadAllBytesAsync(imageFileInfo.FullName);
+		var base64EncodedImage = Convert.ToBase64String(imageBytes);
+
+		// Make sure the model supports image input
+		var tagsResponse = await OllamaClient.Models.GetTagsAsync(default);
+		tagsResponse.Should().NotBeNull();
+		var modelSupportsImages = tagsResponse.Models?.Any(m => string.Equals(m.Name, LlavaModel, StringComparison.OrdinalIgnoreCase)) ?? false;
+		modelSupportsImages.Should().BeTrue("The 'llava' model should be available for this test to run.");
+
+		// Act
+		var request = new GenerateRequest
+		{
+			Model = LlavaModel,
+			Prompt = "Describe this image.",
+			Images = [base64EncodedImage],
+			Stream = false
+		};
+
+		var response = await OllamaClient.Generate.GenerateAsync(request, default);
+
+		// Assert
+		response.Should().NotBeNull();
+		response.Model.Should().Be(LlavaModel);
+		response.Response.Should().NotBeNullOrEmpty();
+		response.Done.Should().BeTrue();
+		response.Context.Should().NotBeNull();
+		response.Context!.Count.Should().BePositive();
+		response.TotalDuration.Should().BePositive();
+		response.LoadDuration.Should().BePositive();
+		response.PromptEvalCount.Should().BePositive();
+		response.PromptEvalDuration.Should().BePositive();
+		response.CreatedAt.Should().NotBeNull();
+		response.CreatedAt!.Value.Should().BeAfter(DateTimeOffset.UtcNow.AddMinutes(-5));
 	}
 }
